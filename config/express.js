f2f92bd7c99c7f2a40e7940194ed64ade4d1c24e
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-11-23 14:52:01 +0300

    feat(cors): add PATCH method to allowed CORS methods

--- config/express.js
+++ config/express.js
@@ -25,7 +25,7 @@ var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Vary', "Origin");   //https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
     res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Methods', 'HEAD,GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'HEAD,GET,PUT,POST,PATCH,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Response-Type, X-Requested-With,origin,accept,Authorization,x-access-token,Last-Modified');
 
     if (req.method == 'OPTIONS') {
