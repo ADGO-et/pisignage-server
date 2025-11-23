9108c0faed149977ad5fd5ea5d8ebfb72eeeb16a
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2025-11-23 13:15:29 +0300

    feat(playlist): enforce 20 seconds duration.

--- app/models/settings.js
+++ app/models/settings.js
@@ -1,28 +1,30 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema
+var mongoose = require("mongoose"),
+  Schema = mongoose.Schema;
 
-var SettingsSchema = new Schema({
-    installation: {type: String , default: "local"},
-    newLayoutsEnable: {type: Boolean , default: false},
-    systemMessagesHide: {type: Boolean, default: false},
-    forceTvOn: {type: Boolean, default: false},
-    disableCECPowerCheck: {type: Boolean, default: false},
-    defaultDuration: {type: Number, default: 10},
-    language: {type: String , default: 'en'},
-    logo: {type: String},
-    url: {type: String},
-    sshPassword: {type: String, default: null},
-    enableLog : {type: Boolean, default: false},
-    hideWelcomeNotice: {type: Boolean, default: false},
-    reportIntervalMinutes:  {type: Number, default: 5},
-    enableYoutubeDl : {type: Boolean, default: true},
+var SettingsSchema = new Schema(
+  {
+    installation: { type: String, default: "local" },
+    newLayoutsEnable: { type: Boolean, default: false },
+    systemMessagesHide: { type: Boolean, default: false },
+    forceTvOn: { type: Boolean, default: false },
+    disableCECPowerCheck: { type: Boolean, default: false },
+    defaultDuration: { type: Number, default: 20 },
+    language: { type: String, default: "en" },
+    logo: { type: String },
+    url: { type: String },
+    sshPassword: { type: String, default: null },
+    enableLog: { type: Boolean, default: false },
+    hideWelcomeNotice: { type: Boolean, default: false },
+    reportIntervalMinutes: { type: Number, default: 5 },
+    enableYoutubeDl: { type: Boolean, default: true },
     authCredentials: {
-        user: {type: String , default: 'pi'},
-        password: {type: String , default: 'pi'}
-    }
-}, {
-    usePushEach: true
-})
+      user: { type: String, default: "pi" },
+      password: { type: String, default: "pi" },
+    },
+  },
+  {
+    usePushEach: true,
+  }
+);
 
-mongoose.model('Settings', SettingsSchema)
-
+mongoose.model("Settings", SettingsSchema);
