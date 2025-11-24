9e1e2dc379219bb2fffc9bad73095b0f9f5ddc3f
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-11-24 12:31:21 +0300

    feat(config): update MongoDB URI for production environment

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,8 @@ module.exports = {
     port: process.env.PORT || 1242,
     mongo: {
         uri: process.env.MONGOLAB_URI ||
-            'mongodb://127.0.0.1:27017/pisignage-server-dev'
+            'mongodb://pisignageUser2:AnotherStrongPass123@127.0.0.1:27017/pisignage-server-dev-2?authSource=pisignage-dev-2'
     }
 };
+
+
