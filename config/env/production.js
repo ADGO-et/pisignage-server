3a0d6e4e4b162493c2f4bebda14caf30a494fac2
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2025-11-24 11:42:17 +0300

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -3,9 +3,9 @@
 module.exports = {
     env: 'production',
     https: false,
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 1242,
     mongo: {
         uri: process.env.MONGOLAB_URI ||
-            'mongodb://127.0.0.1:27017/pisignage-server-dev'
+            'mongodb://pisignageUser2:AnotherStrongPass123@127.0.0.1:27017/pisignage-server-dev-2?authSource=pisignage->
     }
 };
