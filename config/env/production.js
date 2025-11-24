0507ae627147e0df97a2a1b47c25141b3ccecc4b
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-11-24 12:30:12 +0300

    fix: correct MongoDB URI formatting in production configuration

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,8 @@ module.exports = {
     port: process.env.PORT || 1242,
     mongo: {
         uri: process.env.MONGOLAB_URI ||
-            'mongodb://pisignageUser2:AnotherStrongPass123@127.0.0.1:27017/pisignage-server-dev-2?authSource=pisignage->
+            'mongodb://pisignageUser2:AnotherStrongPass123@127.0.0.1:27017/pisignage-server-dev-2?authSource=pisignage-dev-2'
     }
 };
+
+
