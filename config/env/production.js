b3f5d745bbb8139fdfa0631717fd68381234a14c
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-12-06 23:36:48 +0300

    Update for Render deployment with Atlas

--- config/env/production.js
+++ config/env/production.js
@@ -1,11 +1,9 @@
-'use strict';
-
 module.exports = {
     env: 'production',
     https: false,
     port: process.env.PORT || 1242,
     mongo: {
-        uri: process.env.MONGOLAB_URI ||
-            'mongodb://pisignageUser2:AnotherStrongPass123@127.0.0.1:27017/pisignage-server-dev-2?authSource=pisignage-server-dev-2'
+        uri: process.env.MONGODB_URI ||
+            'mongodb+srv://your-atlas-connection-string-here'
     }
 };
